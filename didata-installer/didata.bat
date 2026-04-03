f18e91947b4893785f4166af2955c8455602a1e1
Davis <souzaadavi@gmail.com>
2026-04-02 22:48:13 -0300

    Alteração simples no didata.bat

--- didata-installer/didata.bat
+++ didata-installer/didata.bat
@@ -1,10 +1,19 @@
 @echo off
-REM Script que executa o Didata preservando o diretório de trabalho atual
-REM Passa o diretório atual como variável de ambiente para o Java
 
 setlocal enabledelayedexpansion
-set ORIGINAL_CWD=%CD%
 
-java -jar "%~dp0didata.jar" %*
+set SCRIPT_DIR=%~dp0
+
+set JAVA_EXE=%SCRIPT_DIR%bin\java.exe
+
+if not exist "%JAVA_EXE%" (
+    set JAVA_EXE=%SCRIPT_DIR%jre\bin\java.exe
+)
+
+if not exist "%JAVA_EXE%" (
+    set JAVA_EXE=java.exe
+)
+
+"%JAVA_EXE%" -jar "%SCRIPT_DIR%didata.jar" %*
 
 endlocal
